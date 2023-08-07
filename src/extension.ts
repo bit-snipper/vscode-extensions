34a8d0ba9ee0203d1ae8789c4f71752a17e45276
biubiujch <coolechi@foxmail.com>
2023-08-07 22:09:04 +0800

    update:token输入窗口

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import "isomorphic-fetch";
 import * as vscode from "vscode";
-import { AzureDevOpsAuthenticationProvider } from "./authProvider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,44 +12,40 @@ export function activate(context: vscode.ExtensionContext) {
   // any other extension can use this provider via the `getSession` API.
   // NOTE: when implementing an auth provider, don't forget to register an activation event for that provider
   // in your package.json file: "onAuthenticationRequest:AzureDevOpsPAT"
-  context.subscriptions.push(
-    vscode.authentication.registerAuthenticationProvider(
-      AzureDevOpsAuthenticationProvider.id,
-      "Azure Repos",
-      new AzureDevOpsAuthenticationProvider(context.secrets)
-    )
-  );
+  const storedToken = context.globalState.get('token', '');
 
-  let disposable = vscode.commands.registerCommand("code-snippets.login", async () => {
-    // Get our PAT session.
-    const session = await vscode.authentication.getSession(AzureDevOpsAuthenticationProvider.id, [], {
-      createIfNone: true
+  if (!storedToken) {
+    // If no token is stored, prompt the user to enter one
+    vscode.window.showInputBox({
+      prompt: 'Please enter your token:'
+    }).then(token => {
+      if (token) {
+        context.globalState.update('token', token);
+        vscode.window.showInformationMessage('Token entered: ' + token);
+      }
     });
+  }
 
-    try {
-      // Make a request to the Azure DevOps API. Keep in mind that this particular API only works with PAT's with
-      // 'all organizations' access.
-      const req = await fetch("https://app.vssps.visualstudio.com/_apis/profile/profiles/me?api-version=6.0", {
-        headers: {
-          authorization: `Basic ${Buffer.from(`:${session.accessToken}`).toString("base64")}`,
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          "content-type": "application/json"
+  let disposable = vscode.commands.registerCommand("code-snippets.auth", function () {
+    vscode.window
+      .showInputBox({
+        prompt: "Enter your token:",
+        value: storedToken
+      })
+      .then((token) => {
+        if (token) {
+          context.globalState.update("token", token);
+          vscode.window.showInformationMessage("Token entered: " + token);
         }
       });
-      if (!req.ok) {
-        throw new Error(req.statusText);
-      }
-      const res = (await req.json()) as { displayName: string };
-      vscode.window.showInformationMessage(`Hello ${res.displayName}`);
-    } catch (e: any) {
-      if (e.message === "Unauthorized") {
-        vscode.window.showErrorMessage(
-          "Failed to get profile. You need to use a PAT that has access to all organizations. Please sign out and try again."
-        );
-      }
-      throw e;
-    }
   });
 
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  statusBarItem.text = "😊code-snippets";
+  statusBarItem.command = "code-snippets.auth";
+  statusBarItem.tooltip = "Enter your token";
+  statusBarItem.show();
+
   context.subscriptions.push(disposable);
+  context.subscriptions.push(statusBarItem);
 }
