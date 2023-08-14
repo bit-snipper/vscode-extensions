de6f53d8916bb4bc4819e2c20a578fe9b206c348
janchain <chrislee4642@gmail.com>
2023-08-14 16:27:06 +0800

    udpate:auth

---
+++ src/auth.ts
@@ -0,0 +1,28 @@
+const { SnippetsSDK } = require("snippets-sdk");
+import * as vscode from "vscode";
+
+export const auth = async () => {
+  try {
+    const token = await vscode.window.showInputBox({
+      prompt: "Please enter your token:"
+    });
+    vscode.window.showInformationMessage("Token entered: " + token);
+
+    const useranme = await vscode.window.showInputBox({
+      prompt: "Please enter your userName:"
+    });
+
+    vscode.window.showInformationMessage("Token entered: " + useranme);
+
+    if (token && useranme) {
+      const sdk = new SnippetsSDK(token, useranme);
+      return {
+        token,
+        useranme,
+        sdk
+      };
+    } else {
+      return null;
+    }
+  } catch (e) {}
+};
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import "isomorphic-fetch";
 import * as vscode from "vscode";
+import { auth } from "./auth";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,32 +13,30 @@ export function activate(context: vscode.ExtensionContext) {
   // any other extension can use this provider via the `getSession` API.
   // NOTE: when implementing an auth provider, don't forget to register an activation event for that provider
   // in your package.json file: "onAuthenticationRequest:AzureDevOpsPAT"
-  const storedToken = context.globalState.get('token', '');
+  const storedToken = context.globalState.get("codesnippets-token", "");
 
   if (!storedToken) {
     // If no token is stored, prompt the user to enter one
-    vscode.window.showInputBox({
-      prompt: 'Please enter your token:'
-    }).then(token => {
-      if (token) {
-        context.globalState.update('token', token);
-        vscode.window.showInformationMessage('Token entered: ' + token);
+
+    auth().then((data) => {
+      if (data) {
+        const { token, sdk, useranme } = data;
+        context.globalState.update("codesnippets-token", token);
+        context.globalState.update("codesnippets-username", useranme);
+        console.log(sdk);
       }
     });
   }
 
   let disposable = vscode.commands.registerCommand("code-snippets.auth", function () {
-    vscode.window
-      .showInputBox({
-        prompt: "Enter your token:",
-        value: storedToken
-      })
-      .then((token) => {
-        if (token) {
-          context.globalState.update("token", token);
-          vscode.window.showInformationMessage("Token entered: " + token);
-        }
-      });
+    auth().then((data) => {
+      if (data) {
+        const { token, sdk, useranme } = data;
+        context.globalState.update("codesnippets-token", token);
+        context.globalState.update("codesnippets-username", useranme);
+        console.log(sdk);
+      }
+    });
   });
 
   const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
