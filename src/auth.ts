1d7688a0f0442ed2fcfa2fb1be82725d061d0744
janchain <chrislee4642@gmail.com>
2023-08-15 11:43:13 +0800

    update: 鉴权

--- src/auth.ts
+++ src/auth.ts
@@ -1,25 +1,32 @@
 import { SnippetsSDK } from "snippets-sdk/lib/cjs";
 import * as vscode from "vscode";
+import { v4 as uuidv4 } from "uuid";
 
 export const auth = async () => {
   try {
     const token = await vscode.window.showInputBox({
-      prompt: "Please enter your token:",
+      prompt: "Please enter your token:"
     });
     vscode.window.showInformationMessage("Token entered: " + token);
 
     const useranme = await vscode.window.showInputBox({
-      prompt: "Please enter your userName:",
+      prompt: "Please enter your userName:"
     });
 
     vscode.window.showInformationMessage("Token entered: " + useranme);
 
     if (token && useranme) {
       const sdk = new SnippetsSDK(token, useranme);
+      try {
+   
+      } catch (e) {
+        console.log(e);
+      }
+
       return {
         token,
         useranme,
-        sdk,
+        sdk
       };
     } else {
       return null;
