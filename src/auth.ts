9a9e4def7cb0d1a7561aff64498d3bc36f429095
li changnan <changnan666@gmail.com>
2023-08-15 11:09:12 +0800

    chore: fix sdk

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,15 @@
-import { SnippetsSDK } from "snippets-sdk";
+import { SnippetsSDK } from "snippets-sdk/lib/cjs";
 import * as vscode from "vscode";
 
 export const auth = async () => {
   try {
     const token = await vscode.window.showInputBox({
-      prompt: "Please enter your token:"
+      prompt: "Please enter your token:",
     });
     vscode.window.showInformationMessage("Token entered: " + token);
 
     const useranme = await vscode.window.showInputBox({
-      prompt: "Please enter your userName:"
+      prompt: "Please enter your userName:",
     });
 
     vscode.window.showInformationMessage("Token entered: " + useranme);
@@ -19,7 +19,7 @@ export const auth = async () => {
       return {
         token,
         useranme,
-        sdk
+        sdk,
       };
     } else {
       return null;
